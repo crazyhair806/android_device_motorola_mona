e77fcbbcef164cd8c3afd40c0317c8c22ca721c1
Nicholas Arrizola <64070242+crazyhair806@users.noreply.github.com>
2025-11-08 17:23:18 -0600

    Update omni_mona.mk

--- omni_mona.mk
+++ omni_mona.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from mona device
 $(call inherit-product, device/motorola/mona/device.mk)
 
 PRODUCT_DEVICE := mona
-PRODUCT_NAME := omni_mona
+PRODUCT_NAME := twrp_mona
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := mona
 PRODUCT_MANUFACTURER := motorola
